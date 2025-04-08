8e85fbdda228c0dc02477318290e55cccc3353eb
Raunak <raunakkohli2004@gmail.com>
2025-04-08 11:02:39 +0530

    Initial commit

---
+++ src/Clg_02_program.java
@@ -0,0 +1,8 @@
+public class Clg_02_program {
+    public static void main(String[] args){
+
+        String name = "Raunak";
+        int age = 20;
+        System.out.println("My name is "+name+ "and I am "+age+"  year old");
+    }
+}
