71d6420dbf8b0ddd9e26225c6d22a4e9b148c648
Raunak <raunakkohli2004@gmail.com>
2025-04-13 21:17:35 +0530

    initial commit of college Java programs

--- src/Clg_15_program.java
+++ src/Clg_15_program.java
@@ -3,11 +3,11 @@ public class Clg_15_program {
         String emoji1 = "\uD83D\uDE0A";
         String emoji2 = "\u2728";
          for (int i = 1; i<=6; i++){
-             System.out.println(emoji1+);
+             System.out.println(emoji1+"");
 
         }
          for (int i = 7; i<=9; i++){
-             System.out.println(emoji2+);
+             System.out.println(emoji2+"");
          }
 
     }
