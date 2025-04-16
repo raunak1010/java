ee33167fff78287fa64af1602740ac9e9c3636ab
Raunak <raunakkohli2004@gmail.com>
2025-04-16 13:53:33 +0530

    initial commit of college Java programs

--- src/Clg_16_program.java
+++ src/Clg_16_program.java
@@ -1,8 +1,14 @@
-import java.util.Scanner;
 public class Clg_16_program {
-
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        
+
+        for (int i = 0 ; i < 5; i++){
+            for (int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 5 - i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
