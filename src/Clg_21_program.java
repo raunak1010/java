913d91572bea54da7c83561ecc16dc1c7c4619b2
Raunak <raunakkohli2004@gmail.com>
2025-04-23 20:33:35 +0530

    initial commit of college Java programs

--- src/Clg_21_program.java
+++ src/Clg_21_program.java
@@ -6,8 +6,8 @@ public class Clg_21_program {
         for(int i= 0;i<arr1.length;i++){
             sum[i]=arr1[i]+arr2[i];
         }
-        System.out.println("Sum of two arrays:");
-        for(int i = 0;i<sum.length;i++){
+       System.out.println("Sum of two arrays:");
+      for(int i = 0;i<sum.length;i++){
             System.out.println(sum[i] + "");
         }
     }
